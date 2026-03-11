c8abbf9013c942f344ba87dba90beff5c866a545
Joas Managat <178023837+joashii@users.noreply.github.com>
2026-03-11 10:18:24 +0800

    chore: add all practice files

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import {add as sum, PI} from './math.mjs';
+
+console.log(sum(5, 2)); // 7
+console.log(PI); // 3.14
---
+++ index1.js
@@ -0,0 +1,3 @@
+import {snakeCase} from 'snake-case'
+
+console.log(snakeCase("Hello This Is a snakecase"));
